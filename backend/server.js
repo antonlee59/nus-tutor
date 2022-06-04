2c5cdcd303d28bbb1f92a92fba851d79621d9cb2
antonlee59 <92142956+antonlee59@users.noreply.github.com>
2022-06-05 01:07:39 +0800

    Cleaning up folders

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const connectDB = require("./db/connect");
 
 //Middleware
 app.use(express.json);
 app.use(cors);
 
 //Routes
+
+const port = process.env.PORT || 8000; //Anton's laptop doesnt work on 5000 :(
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGOURI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
