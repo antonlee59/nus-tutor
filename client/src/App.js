23bd7a9acd24d40b2db9ff3c4395a4914444fa9b
antonlee59 <92142956+antonlee59@users.noreply.github.com>
2022-06-05 01:09:58 +0800

    Change folder structure

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+
+//components
+import Login from "./components/Login/Login";
+import LandingPage from './components/LandingPage/LandingPage'
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
