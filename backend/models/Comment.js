4fb7d0358ce97ca57b8d7cc29b98b839a9badcff
Elvern <elvern18@gmail.com>
2022-06-11 23:51:43 +0800

    Comment and Post Model w some functions

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,5 +1,44 @@
 const mongoose = require("mongoose");
 
-const CommentSchema = new mongoose.Schema({});
+const commentSchema = new mongoose.Schema({  
+    text: {
+        type: String,
+    },
+    created: {
+        type: Date,
+        default: Date.now(),
+    },
+    author: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+    },
+    username: {
+        type: String,
+    },
+    score: {
+        type: Number,
+        default: 0,
+    },
+    upvotedBy: {
+        type: Array,
+    },
+    downvotedBy: {
+        type: Array,
+    },
+});
 
-module.exports = mongoose.model("Comment", CommentSchema);
+commentSchema.methods.upvoteComment = function upvoteComment(comment) {
+    var newComment = comment;
+    newComment.score += 1;
+    // newComment.upvotedBy.push(User)
+    return newComment;
+}
+  
+commentSchema.methods.downvoteComment = function downvoteComment(comment) {
+    var newComment = comment;
+    newComment.score -= 1;
+    // newComment.downvotedBy.push(User)
+    return newComment;
+}
+
+module.exports = mongoose.model("Comment", commentSchema);
--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,7 +1,67 @@
 const mongoose = require('mongoose')
 
-const PostSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
+    created: {
+        type: Date,
+        default: Date.now(),
+    },
+    author: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+    },
+    username: {
+        type: String,
+    },
+    score: {
+        type: Number,
+        default: 0,
+    },
+    comments: {
+        type: Array,
+    },
+    upvotedBy: {
+        type: Array,
+    },
+    downvotedBy: {
+        type: Array,
+    },
+});
 
-})
+postSchema.methods.upvotePost = function upvotePost(post) {
+    var newPost = post;
+    newPost.score += 1;
+    // newPost.upvotedBy.push(User);
+    return newPost;
+}
+  
+postSchema.methods.downvotePost = function downvotePost(post) {
+    var newPost = post;
+    newPost.score -= 1;
+    // newPost.downvotedBy.push(User);
+    return newPost;
+}
+
+postSchema.methods.commentOnPost = function commentOnPost(content) {
+    const comment = {
+        text: content,
+        created: Date.now(),
+        score: 0,
+        upvotedBy: [],
+        downvotedBy: [],
+        // author: User,
+        // username: User.username,
+    }
+    return comment;
+}
 
 module.exports = mongoose.model('Post', PostSchema)
