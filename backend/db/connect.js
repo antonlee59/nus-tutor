f85578a29dae2bf0c5249cfff87be67aa9b7f14f
Elvern <elvern18@gmail.com>
2022-06-23 20:15:42 +0800

    connected backend

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,7 +1,46 @@
+// const { application, Router } = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
+const express = require("express");
+const Router = require("./../routes/routes.js")
+const app = express();
 
-const connectDB = (url) => {
-  return mongoose.connect(url);
-};
+const uri = process.env.URI;
+console.log(uri);
 
-module.exports = connectDB;
+
+mongoose.connect(
+  uri, 
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+    
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.use(Router);
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+})
+
+
+// const username = "SHA2"
+// const password = "SHA2"
+// const cluster = "cluster0.9qrjsk4"
+// const dbname = "Orbital"
+
+
+// const connectDB = (uri) => {
+//   return mongoose.connect(uri),
+//   {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true
+//    };
+// };
+
+// module.exports = connectDB;
