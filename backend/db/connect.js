22baf0044299352c8bce95563f460c9279c69c5a
Elvern <elvern18@gmail.com>
2022-06-23 21:05:56 +0800

    connect p2

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -5,42 +5,42 @@ const express = require("express");
 const Router = require("./../routes/routes.js")
 const app = express();
 
-const uri = process.env.URI;
+const uri = process.env.MONGO_URI;
 console.log(uri);
 
+const connectDB = (uri) => {
+  return mongoose.connect(uri),
+  {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+   };
+};
 
-mongoose.connect(
-  uri, 
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  );
+module.exports = connectDB;
+
+
+
+// mongoose.connect(
+//   uri, 
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     }
+//   );
     
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//   console.log("Connected successfully");
+// });
 
-app.use(Router);
-app.listen(3000, () => {
-  console.log("Server is running at port 3000");
-})
+// app.use(Router);
+// app.listen(3000, () => {
+//   console.log("Server is running at port 3000");
+// })
 
 
 // const username = "SHA2"
 // const password = "SHA2"
 // const cluster = "cluster0.9qrjsk4"
 // const dbname = "Orbital"
-
-
-// const connectDB = (uri) => {
-//   return mongoose.connect(uri),
-//   {
-//    useNewUrlParser: true,
-//    useUnifiedTopology: true
-//    };
-// };
-
-// module.exports = connectDB;
